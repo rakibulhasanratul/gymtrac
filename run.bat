82f3c6f8b92740c190f522cd5c45694d0b5cdc87
Rakibul Hasan Ratul <89402619+rakibulhasanratul@users.noreply.github.com>
2026-08-23 06:20:40 +0000

    fix: update build command to include module and utility source files

--- run.bat
+++ run.bat
@@ -7,6 +7,6 @@ if "%~1"=="test" (
     gcc -Wall -Wextra -pedantic -g -fmacro-prefix-map=src/= test\*.c test\**\*.c src\**\*.c -o build\test_runner.exe -lm
     build\test_runner.exe
 ) else (
-    gcc -Wall -Wextra -pedantic -g -fmacro-prefix-map=src/= src\*.c src\**\*.c -o build\gymtrac.exe -lm
+    gcc -Wall -Wextra -pedantic -g -fmacro-prefix-map=src/= src\*.c src\modules\*.c src\utils\*.c -o build\gymtrac.exe -lm
     build\gymtrac.exe
 )
